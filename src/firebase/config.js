6aa479863721f505118c84c1ca85c612152319db
tatsuki1107 <tatsuki11070522@icloud.com>
2022-08-03 15:34:09 +0900

    create dev environment

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAZzUVyprTrDGWjS41S4KpqX-FZEA39CL0",
+  authDomain: "memo-demo-ab619.firebaseapp.com",
+  projectId: "memo-demo-ab619",
+  storageBucket: "memo-demo-ab619.appspot.com",
+  messagingSenderId: "1067916742732",
+  appId: "1:1067916742732:web:0225443315d64d656164e9",
+  measurementId: "G-RC9V9GF0YP"
+};
+
+export default firebaseConfig;
